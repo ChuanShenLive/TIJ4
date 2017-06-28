f028774e1cdf6dd7a607339d2548452a533b5d8e
ChuanShen <chuanshen@live.cn>
2017-06-28 23:34:32 +0800

    localInnerClass

--- code/TIJ4/src/innerclasses/InheritInner.java
+++ code/TIJ4/src/innerclasses/InheritInner.java
@@ -9,6 +9,12 @@ class WithInner {
 public class InheritInner extends WithInner.Inner {
 	// InheritInner() {} // Won't compile
 	InheritInner(WithInner wi) {
+		//enclosingClassReference.super();
 		wi.super();
 	}
-}
+	
+	public static void main(String[] args) {
+		WithInner wi = new WithInner();
+		InheritInner ii = new InheritInner(wi);
+	}
+} ///:~
