ef34a9521085af0c0f2ce42495180e427cd9ccb0
ChuanShenLive <chuanshen@live.cn>
2017-09-19 22:58:43 +0800

    14.3.2

---
+++ code/TIJ4/src/typeinfo/PetCount2.java
@@ -0,0 +1,13 @@
+//: typeinfo/PetCount2.java
+package typeinfo;
+
+import typeinfo.pets.Pets;
+
+public class PetCount2 {
+	public static void main(String[] args) {
+		PetCount.countPets(Pets.creator);
+	}
+} /* Output:
+Rat Manx Cymric Mutt Pug Cymric Pug Manx Cymric Rat EgyptianMau Hamster EgyptianMau Mutt Mutt Cymric Mouse Pug Mouse Cymric 
+{EgyptianMau=2, Pug=3, Rat=2, Mouse=2, Cat=9, Cymic=5, Manx=7, Rodent=5, Mutt=3, Dog=6, Pet=20, Hamster=1}
+*///:~
