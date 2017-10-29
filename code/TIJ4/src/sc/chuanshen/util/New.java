c5edaaf3dca747923be5f43c72e7dc40177c77af
ChuanShenLive <chuanshen@live.cn>
2017-10-29 23:19:13 +0800

    15.4.3

--- code/TIJ4/src/sc/chuanshen/util/New.java
+++ code/TIJ4/src/sc/chuanshen/util/New.java
@@ -34,6 +34,7 @@ public class New {
 	}
 	
 	// Examples:
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Map<String, List<String>> sls = New.map();
 		List<String> ls = New.list();
