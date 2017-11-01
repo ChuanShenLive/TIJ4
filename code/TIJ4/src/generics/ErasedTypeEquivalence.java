88d8886ad18ac16eecb2344ad982e4f34aa9c2bd
ChuanShenLive <chuanshen@live.cn>
2017-11-01 23:20:47 +0800

    15.7

---
+++ code/TIJ4/src/generics/ErasedTypeEquivalence.java
@@ -0,0 +1,15 @@
+//: generics/ErasedTypeEquivalence.java
+package generics;
+
+import java.util.ArrayList;
+
+public class ErasedTypeEquivalence {
+	@SuppressWarnings("rawtypes")
+	public static void main(String[] args) {
+		Class c1 = new ArrayList<String>().getClass();
+		Class c2 = new ArrayList<Integer>().getClass();
+		System.out.println(c1 == c2);
+	}
+} /*Output:
+true
+*///:~
---
+++ code/TIJ4/src/generics/Manipulation.java
@@ -0,0 +1,25 @@
+//: generics/Manipulation.java
+// {CompileTimeError} (Won't compile)
+package generics;
+
+class Manipulator<T extends HasF> {
+	private T obj;
+	public Manipulator(T x) {
+		obj = x;
+	}
+	
+	// Error: connot find symbol: method f():
+	public void manipulate() {
+		obj.f();
+	}
+}
+
+
+public class Manipulation {
+	public static void main(String[] args) {
+		HasF hf = new HasF();
+		Manipulator<HasF> manipulator =
+				new Manipulator<HasF>(hf);
+		manipulator.manipulate();
+	}
+} ///:~
