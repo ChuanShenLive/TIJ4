b473a721060b22e558267df3ad11de502c8636c8
ChuanShenLive <chuanshen@live.cn>
2017-11-02 11:10:49 +0800

    15.7.3

---
+++ code/TIJ4/src/generics/ErasureAndInheritance.java
@@ -0,0 +1,34 @@
+//: generics/ErasureAndInheritance.java
+package generics;
+
+class GenreicBase<T> {
+    private T element;
+
+    public void set(T arg) {
+        element = arg;
+    }
+
+    public T get() {
+        return element;
+    }
+}
+
+class Derived1<T> extends GenreicBase<T> {
+}
+
+class Derived2<T> extends GenreicBase<T> {
+}
+
+// class Derived3 extends GenericBase<?> {}
+// Strange error:
+//      unexpected type found : ?
+//      required: class or interface without bounds
+
+public class ErasureAndInheritance {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        Derived2 d2 = new Derived2();
+        Object obj = d2.get();
+        d2.set(obj);    // Warning here!
+    }
+} ///:~
