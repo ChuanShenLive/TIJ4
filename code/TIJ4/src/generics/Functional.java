91d5a03c8795948c4a9d4fd534bcff93e6e05bc4
ChuanShenLive <chuanshen@live.cn>
2017-12-29 23:31:01 +0800

    15.18

--- code/TIJ4/src/generics/Functional.java
+++ code/TIJ4/src/generics/Functional.java
@@ -3,9 +3,14 @@ package generics;
 
 import static net.mindview.util.Print.*;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Different types of function objects:
 interface Combiner<T> {
@@ -82,6 +87,114 @@ public class Functional {
 	}
 	
 	static class IntegerSubtracter implements Combiner<Integer> {
-		public Integer combine()
+		public Integer combine(Integer x, Integer y) {
+			return x - y;
+		}
 	}
-}
+	
+	static class BigDecimalAdder implements Combiner<BigDecimal> {
+		public BigDecimal combine(BigDecimal x, BigDecimal y) {
+			return x.add(y);
+		}
+	}
+	
+	static class BigIntegerAdder implements Combiner<BigInteger> {
+		public BigInteger combine(BigInteger x, BigInteger y) {
+			return x.add(y);
+		}
+	}
+	
+	static class AtomicLongAdder implements Combiner<AtomicLong> {
+		public AtomicLong combine(AtomicLong x, AtomicLong y) {
+			// Not clear whether this is meaningful:
+			return new AtomicLong(x.addAndGet(y.get()));
+		}
+	}
+	
+	// We can even make a UnaryFunction with an "ulp"
+	// (Units in the last place):
+	static class BigDecimalUlp implements UnaryFunction<BigDecimal, BigDecimal> {
+		public BigDecimal function(BigDecimal x) {
+			return x.ulp();
+		}
+	}
+	
+	static class GreaterThan<T extends Comparable<T>> implements UnaryPredicate<T> {
+		private T bound;
+		public GreaterThan(T bound) {
+			this.bound = bound;
+		}
+		public boolean test(T x) {
+			return x.compareTo(bound) > 0;
+		}
+	}
+	
+	static class MultiplyingIntegerCollector implements Collector<Integer> {
+		private Integer val =1;
+		public Integer function(Integer x) {
+			val *= x;
+			return val;
+		}
+		public Integer result() {
+			return val;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// Generics, varargs & boxing working together:
+		List<Integer> li = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+		Integer result = reduce(li, new IntegerAdder());
+		print(result);
+		
+		result = reduce(li, new IntegerSubtracter());
+		print(result);
+		
+		print(filter(li, new GreaterThan<Integer>(4)));
+		
+		print(forEach(li, new MultiplyingIntegerCollector()).result());
+		
+		MathContext mc = new MathContext(7);
+		List<BigDecimal> lbd = Arrays.asList(
+				new BigDecimal(1.1, mc), new BigDecimal(2.2, mc),
+				new BigDecimal(3.3, mc), new BigDecimal(4.4, mc));
+		BigDecimal rbd = reduce(lbd, new BigDecimalAdder());
+		print(rbd);
+		
+		print(filter(lbd, new GreaterThan<BigDecimal>(new BigDecimal(3))));
+		
+		// Use the prime-generation facility of BigInteger:
+		List<BigInteger> lbi = new ArrayList<BigInteger>();
+		BigInteger bi = BigInteger.valueOf(11);
+		for(int i = 0; i < 11; i++) {
+			lbi.add(bi);
+			bi = bi.nextProbablePrime();
+		}
+		print(lbi);
+		
+		BigInteger rbi = reduce(lbi, new BigIntegerAdder());
+		print(rbi);
+		
+		// The sum of this list of primes is also prime:
+		print(rbi.isProbablePrime(5));
+		
+		List<AtomicLong> lal = Arrays.asList(
+				new AtomicLong(11), new AtomicLong(47),
+				new AtomicLong(74), new AtomicLong(133));
+		AtomicLong ral = reduce(lal, new AtomicLongAdder());
+		print(ral);
+		
+		print(transform(lbd, new BigDecimalUlp()));
+	}
+} /*Output:
+28
+-26
+[5, 6, 7]
+5040
+11.000000
+[3.300000, 4.400000]
+[11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+311
+true
+265
+[0.000001, 0.000001, 0.000001, 0.000001] 
+*///:~
